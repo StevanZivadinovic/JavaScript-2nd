197f01f63044046bb5b2005058453103d84a135c
Stevan <stevanzivadinovic@outlook.com>
2021-06-08 14:38:50 +0200

    a

--- Noctour website/index.js
+++ Noctour website/index.js
@@ -11,17 +11,17 @@ function isInViewport(element) {
 
 addEventListener('scroll',e=>{
 
-    if(isInViewport(document.querySelector('.heading-primary-main')) && window.scrollY==0){
+    if(isInViewport(document.querySelector('.heading-primary--main')) && window.scrollY==0){
         console.log('haj')
-        document.querySelector('.heading-primary-main').style.animation = 'moveInLeft 5s ease-in';
-        document.querySelector('.heading-primary-sub').style.animation = 'moveInRight 5s ease-in';
-        document.querySelector('.logo').style.animation = 'moveInLeft 5s ease-in';
+        document.querySelector('.heading-primary--main').style.animation = 'moveInLeft 5s ease-in';
+        document.querySelector('.heading-primary--sub').style.animation = 'moveInRight 5s ease-in';
+        document.querySelector('.header__logo').style.animation = 'moveInLeft 5s ease-in';
      
        
     }else{
       
-        document.querySelector('.heading-primary-main').style.animation = 'none';
-        document.querySelector('.heading-primary-sub').style.animation = 'none';
-        document.querySelector('.logo').style.animation = 'none';
+        document.querySelector('.heading-primary--main').style.animation = 'none';
+        document.querySelector('.heading-primary--sub').style.animation = 'none';
+        document.querySelector('.header__logo').style.animation = 'none';
     }
 })
