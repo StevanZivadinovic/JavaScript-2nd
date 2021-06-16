c9294b4511a945585eb83e40c870d2de791ba0a2
Stevan <stevanzivadinovic@outlook.com>
2021-06-16 11:45:40 +0200

    change

---
+++ Asymmetry website/script.js
@@ -0,0 +1,5 @@
+let button = document.querySelector('.mainIntroduction__mainDiv--button');
+button.addEventListener('click',e=>{
+    console.log('haj');
+    window.location.href = './memory.html';
+})
