1670cad49ba37c7fae120e5b87d5d7043c134ac7
Stevan <stevanzivadinovic@outlook.com>
2021-06-17 22:54:39 +0200

    change

--- Asymmetry website/scripts/scriptSwipe.js
+++ Asymmetry website/scripts/scriptSwipe.js
@@ -4,3 +4,15 @@ document.querySelectorAll('.swipe__buttons')
         window.location.href = './registration.html';
 })
     })
+
+    let a=false;
+    document.querySelector('body').addEventListener('dragend',(e)=>{
+        window.location.href = './registration.html';
+       console.log('haj',e)
+    })
+      
+   
+    // document.querySelector('body').addEventListener('mousemove',()=>console.log(),false)
+    // document.querySelector('body').addEventListener('mouseup',()=>console.log(),false)
+
+    
