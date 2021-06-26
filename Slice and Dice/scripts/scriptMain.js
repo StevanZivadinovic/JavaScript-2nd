66b790d6b312565479cc384a382ae42792de84a0
Stevan <stevanzivadinovic@outlook.com>
2021-06-26 19:51:39 +0200

    change

--- Slice and Dice/scripts/scriptMain.js
+++ Slice and Dice/scripts/scriptMain.js
@@ -24,5 +24,6 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "flex";
+ 
   dots[slideIndex-1].className += " active";
 }
