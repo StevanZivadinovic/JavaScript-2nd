1c553fb5a72bf4af98b50733be865544ad469add
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-01-09 22:41:26 +0100

    change

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,36 +1,58 @@
-let history = document.querySelector('#history-value');
-let output = document.querySelector('#output-value');
+let history = document.querySelector("#history-value");
+let output = document.querySelector("#output-value");
 
+document.querySelector("#clear").addEventListener("click", (e) => {
+  history.innerHTML = "";
+});
 
+try {
+  document.querySelector("#keyboard").addEventListener("click", (e) => {
+    console.log(e.target.textContent);
 
-document.querySelector('#clear').addEventListener('click',e=>{
-    history.innerHTML = ''
-})
+    if (
+      e.target.textContent != "C" &&
+      e.target.textContent != "CE" &&
+      e.target.textContent != "="
+    )
+      history.innerHTML += e.target.textContent.toLocaleString("en");
 
-
-try{
-
-document.querySelector('#keyboard').addEventListener('click',e=>{
-    console.log(e.target.textContent)
-if(e.target.textContent != 'C') document.querySelector('#history-value').innerHTML += e.target.textContent;
-if(e.target.textContent == '='){
-    console.log(history.textContent.slice(0, -1))
-    let text = history.textContent.slice(0, -1)
-    console.log(typeof text)
-    let c = eval(text)
-    console.log(c)
-    output.innerHTML = c;
-    history.innerHTML = ''
-
+    if (e.target.textContent == "=") {
+      output.textContent = "";
+      console.log(history.textContent.slice(0, -1));
+      let text = history.textContent.slice(0, -1);
+      console.log(typeof text);
+      console.log(text.includes("x"));
+      if (text.includes("x")) {
+        // let c = text.indexOf('x');
+        console.log(text);
+        let noviText = text.replace(/x/g, "*");
+        console.log(noviText);
+        let b = eval(noviText);
+        output.innerHTML = b.toLocaleString("en");
+        history.innerHTML = "";
+      } else if (text.includes("÷")) {
+        console.log(text);
+        let noviText = text.replace(/÷/g, "/");
+        console.log(noviText);
+        let b = eval(noviText);
+        output.innerHTML = b.toLocaleString("en");
+        history.innerHTML = "";
+      } else {
+        let c = eval(text);
+        console.log(c);
+        output.innerHTML = c.toLocaleString("en");
+        history.innerHTML = "";
+      }
+    }
+  });
+} catch (e) {
+  console.log(e);
 }
-    
-}
 
-
-)
-}catch(e){
-    console.log(e)
-}
-
-
-document.querySelector
+let CE = document.querySelector("#backspace");
+CE.addEventListener("click", (e) => {
+  history.textContent = history.textContent.substring(
+    0,
+    history.textContent.length - 1
+  );
+});
